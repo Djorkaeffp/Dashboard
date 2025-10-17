90cc740ace43f06445dd4fb20e12a811a7e266b8
Djorkaeffp <djorkaeffoliveira7@gmail.com>
2025-10-17 18:26:47 -0300

    vercel

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/turma')
+def turma():
+    return render_template('turma.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
