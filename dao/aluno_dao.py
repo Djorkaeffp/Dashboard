36ce6b442f2259c66ae4f41a1fb888d5a9bf9e1d
Djorkaeffp <djorkaeffoliveira7@gmail.com>
2025-10-23 13:20:29 -0300

    vercel

--- dao/aluno_dao.py
+++ dao/aluno_dao.py
@@ -1,13 +1,17 @@
 from dao.db_config import get_connection
 
+
 class alunoDAO:
 
-    sqlSelect = ' SELECT id, nome, idade, cidade FROM aluno'
+
+    sqlSelect = 'SELECT id, nome, idade, cidade FROM aluno'
+
 
     def listar(self):
+
         conn = get_connection()
+
         cursor = conn.cursor()
+
         cursor.execute(self.sqlSelect)
-        lista = cursor.fetchall()
-        conn.close()
-        return lista
+        return cursor.fetchall()
