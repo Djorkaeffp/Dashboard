a6139376602aa40bb5243da142ec600f910876ba
Djorkaeffp <djorkaeffoliveira7@gmail.com>
2025-10-23 12:42:34 -0300

    vercel

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from dao.aluno_dao import alunoDAO
 
 app = Flask(__name__)
 
@@ -15,6 +15,14 @@ def turma():
 @app.route('/cadastrar')
 def cadastrar():
     return render_template('cadastrar.html')
+
+@app.route('/aluno')
+def listar_aluno():
+    dao = alunoDAO()
+    lista = dao.listar
+    return render_template('aluno/lista.html',lista=lista)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ dao/aluno_dao.py
@@ -0,0 +1,13 @@
+from dao.db_config import get_connection
+
+class alunoDAO:
+    
+    sqlSelect = ' SELECT id, nome, idade, cidade FROM aluno'
+
+    def listar(self):
+        conn = get_connection()
+        cursor = conn.curso()
+        cursor.execute(self.sqlSelect)
+        lista = cursor.fetchall()
+        conn.close()
+        return lista
