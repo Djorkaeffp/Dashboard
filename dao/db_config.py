0328d225cc09d8ea08ba943b5a91c6c1b6226083
Djorkaeffp <djorkaeffoliveira7@gmail.com>
2025-11-18 13:57:05 -0300

    vercel

--- dao/db_config.py
+++ dao/db_config.py
@@ -1,7 +1,7 @@
 import sqlite3
-
-DB_PATH = "banco_escola.db"
+import psycopg2
+DB_PATH = "postgresql://neondb_owner:npg_ly2KSID7XiTU@ep-autumn-truth-ahra4w2g-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
 def get_connection():
-    conn = sqlite3.connect(DB_PATH)
+    conn = psycopg2.connect(DB_PATH)
     return conn
